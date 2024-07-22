73a7d05c8373bdc670966e6ce372cbf321c10d08
SeokSeungMin <seoksmin@gmail.com>
2024-07-22 19:33:32 +0900

    Initial commit

---
+++ src/main/java/com/example/account/dto/AccountDto.java
@@ -0,0 +1,30 @@
+package com.example.account.dto;
+
+import com.example.account.domain.Account;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountDto {
+    private Long userId;
+    private String accountNumber;
+    private Long balace;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+
+    public static AccountDto fromEntity(Account account) {
+        return AccountDto.builder()
+                .userId(account.getAccountUser().getId())
+                .accountNumber(account.getAccountNumber())
+                .balace(account.getBalance())
+                .registeredAt(account.getRegisteredAt())
+                .unregisteredAt(account.getUnRegisteredAt())
+                .build();
+    }
+}
